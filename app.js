77c34319eb9ed5fa8d1770836167bab3ac85e45c
Hamza Husic <hamzahusic76@gmail.com>
2022-07-14 22:34:03 +0200

    Fixed some bugs and added animation to some elements

--- app.js
+++ app.js
@@ -5,7 +5,31 @@ const lineOne = document.getElementById('line1');
 const lineTwo = document.getElementById('line2');
 const lineThree = document.getElementById('line3');
 
+const about = document.getElementById('About');
+const projects = document.getElementById('Projects');
+const contact = document.getElementById('contact');
+//Ove sve iznad konstante su za navbar
+
 hamburger.addEventListener('click', () => {
+    lista.classList.toggle('active');
+    lineOne.classList.toggle('okreniLijevo');
+    lineTwo.classList.toggle('nema');
+    lineThree.classList.toggle('okreniDesno');
+})
+
+about.addEventListener('click', () => {
+    lista.classList.toggle('active');
+    lineOne.classList.toggle('okreniLijevo');
+    lineTwo.classList.toggle('nema');
+    lineThree.classList.toggle('okreniDesno');
+})
+projects.addEventListener('click', () => {
+    lista.classList.toggle('active');
+    lineOne.classList.toggle('okreniLijevo');
+    lineTwo.classList.toggle('nema');
+    lineThree.classList.toggle('okreniDesno');
+})
+contact.addEventListener('click', () => {
     lista.classList.toggle('active');
     lineOne.classList.toggle('okreniLijevo');
     lineTwo.classList.toggle('nema');
