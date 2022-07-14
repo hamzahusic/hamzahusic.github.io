213f509a63895216dd6dc45f56b7d75acdd3fb70
Hamza Husic <hamzahusic76@gmail.com>
2022-07-14 12:11:46 +0200

    Fixed padding in projects area and added animation to hamburger menu

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 
 const hamburger = document.getElementById('hamburger');
 const lista = document.getElementById('lista');
+const lineOne = document.getElementById('line1');
+const lineTwo = document.getElementById('line2');
+const lineThree = document.getElementById('line3');
 
 hamburger.addEventListener('click', () => {
     lista.classList.toggle('active');
+    lineOne.classList.toggle('okreniLijevo');
+    lineTwo.classList.toggle('nema');
+    lineThree.classList.toggle('okreniDesno');
 })
