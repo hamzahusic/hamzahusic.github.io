0542a225d6bea40b6151c9307e28aa249504e9f9
Hamza Husic <hamzahusic76@gmail.com>
2022-08-26 15:54:01 +0200

    Fixed some js code

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
 
-const hamburger = document.getElementById('hamburger');
+const hamburger = document.querySelectorAll('#hamburger');
 const lista = document.getElementById('lista');
 const lineOne = document.getElementById('line1');
 const lineTwo = document.getElementById('line2');
 const lineThree = document.getElementById('line3');
 
-const about = document.getElementById('About');
-const projects = document.getElementById('Projects');
-const contact = document.getElementById('contact');
 //Ove sve iznad konstante su za navbar
 
 let backToTop = document.getElementById('backOnTop');
 
-hamburger.addEventListener('click', () => {
+hamburger.forEach(el=>{
+    el.addEventListener('click', () => {
     lista.classList.toggle('active');
     lineOne.classList.toggle('okreniLijevo');
     lineTwo.classList.toggle('nema');
     lineThree.classList.toggle('okreniDesno');
 })
-
-about.addEventListener('click', () => {
-    lista.classList.toggle('active');
-    lineOne.classList.toggle('okreniLijevo');
-    lineTwo.classList.toggle('nema');
-    lineThree.classList.toggle('okreniDesno');
-})
-projects.addEventListener('click', () => {
-    lista.classList.toggle('active');
-    lineOne.classList.toggle('okreniLijevo');
-    lineTwo.classList.toggle('nema');
-    lineThree.classList.toggle('okreniDesno');
-})
-contact.addEventListener('click', () => {
-    lista.classList.toggle('active');
-    lineOne.classList.toggle('okreniLijevo');
-    lineTwo.classList.toggle('nema');
-    lineThree.classList.toggle('okreniDesno');
-})
+});
 
 window.onscroll = function(){scrollFunction()};
 
